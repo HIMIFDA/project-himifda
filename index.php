15bf446ddb51109501da24a202a16cba0d8d6e73
Nadia Salsabil <salsabilnadia7@gmail.com>
2018-05-16 23:47:00 +0700

    Himifda

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Himifda</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg  fixed-top" style="background-color: #1e90ff;">
+      <div class="container">
+        <a class="navbar-brand" href="index.html"><img src="images/4.png" width="25px" height="25px"><font color= "white"> <b> HIMIFDA </b></font></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" style="background-color: white;"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive" >
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#"><font color= "white"> <b>Beranda</b></font></a>
+            </li>
+           
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <font color= "white"> <b>Profile</b></font>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="#">Sejarah</a>
+                <a class="dropdown-item" href="#">Visi&Misi</a>
+                <a class="dropdown-item" href="#">Pengurus</a>
+                 <a class="dropdown-item" href="#">Struktural</a>
+                  <a class="dropdown-item" href="#">Prestasi</a>
+               
+              </div>
+            </li>
+
+             <li class="nav-item">
+              <a class="nav-link" href="about.html"><font color= "white"> <b>Event</b></font></a>
+            </li>
+            
+              <li class="nav-item dropdown" >
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+              <font color= "white"> <b> Akademik</b></font>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="portfolio-1-col.html">Mata Kuliah Informatika</a>
+                <a class="dropdown-item" href="portfolio-2-col.html">Kalender AKademik</a>
+                <a class="dropdown-item" href="portfolio-3-col.html">Info Beasiswa</a>
+                 <a class="dropdown-item" href="portfolio-3-col.html">Portal Unsada</a>
+                  <a class="dropdown-item" href="portfolio-3-col.html">Tutorial</a>
+               
+               
+              </div>
+            </li>
+           
+             
+             <li class="nav-item">
+              <a class="nav-link" href="about.html"><font color= "white"> <b>FAQ</b></font></a>
+            </li>
+
+             <li class="nav-item">
+              <a class="nav-link" href="about.html"><font color= "white"> <b>Contact</b></font></a>
+            </li>
+           
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('images/1.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3> HIMIFDA </h3>
+              <p>Himpunan Informatika Universitas Darma Persada</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('images/2.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3> HIMIFDA </h3>
+           
+              <p>Himpunan Informatika Universitas Darma Persada</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('images/1.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3> HIMIFDA </h3>
+            
+              <p>Himpunan Informatika Universitas Darma Persada</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container"><br>
+    
+
+      <h3 class="my-4"><center><b>Himpunan Informatika Universitas Darma Persada</b></center></h3><hr />
+
+      <!-- Marketing Icons Section -->
+      <div class="row">
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Berita Himifda</h4>
+            <div class="card-body">
+              <p class="card-text">Himifda Singkatan dari Universitas Darma Persada yang terletak di Fakultas Teknik Lt.4 . </p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Acara Hifasco</h4>
+            <div class="card-body">
+              <p class="card-text">Hifasco singkatan dari Himifda Seminar and Competition</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Berita Himifda</h4>
+            <div class="card-body">
+              <p class="card-text">Himifda Singkatan dari Universitas Darma Persada yang terletak di Fakultas Teknik Lt.4 .</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+ <div class="row">
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Berita Himifda</h4>
+            <div class="card-body">
+              <p class="card-text">Himifda Singkatan dari Universitas Darma Persada yang terletak di Fakultas Teknik Lt.4 . </p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Acara Hifasco</h4>
+            <div class="card-body">
+              <p class="card-text">Hifasco singkatan dari Himifda Seminar and Competition</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Berita Himifda</h4>
+            <div class="card-body">
+              <p class="card-text">Himifda Singkatan dari Universitas Darma Persada yang terletak di Fakultas Teknik Lt.4 .</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>      <!-- /.row -->
+
+      <!-- Portfolio Section -->
+     
+      <!-- /.row -->
+
+      <!-- Features Section -->
+      <div class="row">
+        <div class="col-lg-6">
+         
+         
+          
+        </div>
+        <div class="col-lg-6">
+         
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Call to Action Section -->
+      <div class="row mb-4">
+        <div class="col-md-8">
+         
+        </div>
+        <div class="col-md-4">
+         
+        </div>
+      </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; HIMIFDA 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
